ed88373e656338cdc3f71442c867316911ac714e
Dmitry Brant <me@dmitrybrant.com>
2025-09-06 20:50:45 -0400

    Clarifying comments.

--- insert_parallel.sh
+++ insert_parallel.sh
@@ -9,15 +9,21 @@
 insmod ./ftape-core.ko ft_soft_retries=1 ft_ignore_ecc_err=1 ft_tracings=5,5,5,5,5 ft_fdc_driver=bpck-fdc,none,none,none # ft_fdc_driver=ftape-internal,bpck-fdc:trakker,none,none ft_tracings=3,3,3,3,3
 insmod ./zftape.ko # ft_major_device_number=27 # ${27-FT_MAJOR}
 
+# The following used to be necessary, but recent kernels
+# auto-load the parport modules as needed. If you have a
+# parallel port, it should be auto-detected and available
+# through parport, which our modules will use.
+# ---------------------------------------
 # remove any running instances of parport or related modules
 #rmmod lp
 #rmmod parport_pc
 #rmmod ppdev
 #rmmod parport
-
 # and now install our stuff.
 #modprobe parport_pc io=0x378 irq=7
 #modprobe parport
+# ---------------------------------------
+
 insmod ./ftape-parport.ko
 insmod ./ftape-bpck.ko
 
